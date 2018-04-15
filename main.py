f13d7da6f5b36a87d371ccbb05ef5c2e4b26ea09
Slavaqq <slavaqq@gmail.com>
2018-04-15 21:23:47 +0200

    Basic movement and better character switching

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ class Game(arcade.Window):
         self.erika_sprite = None
         self.quentin_sprite = None
         self.walls = None
-        self.physics_engine = None
+        self.physics_erika = None
+        self.physics_quentin = None
+        self.erika_active = True
+        self.quentin_active = False
 
     def setup(self):
         arcade.set_background_color(arcade.color.AMAZON)
@@ -30,7 +33,7 @@ class Game(arcade.Window):
         self.erika_list.append(self.erika_sprite)
         self.quentin_sprite = arcade.Sprite("player_idle.png", .9)
         self.quentin_sprite.center_x = 90
-        self.quentin_sprite.center_y = 350
+        self.quentin_sprite.center_y = 250
         self.quentin_sprite.alpha = 0
         self.quentin_list.append(self.quentin_sprite)
         
@@ -51,12 +54,18 @@ class Game(arcade.Window):
         wall = arcade.Sprite("stoneMid.png", .25, center_x=752, center_y=80)
         self.walls.append(wall)
     
-        for x in range(16, 161, 32):
+        for x in range(48, 161, 32):
             wall = arcade.Sprite("stoneMid.png", .25, center_x=x, center_y=176)
             self.walls.append(wall)
 
-        self.physics_engine = arcade.physics_engines.PhysicsEnginePlatformer(self.erika_sprite, self.walls)
+        self.physics_erika = arcade.physics_engines.PhysicsEnginePlatformer(self.erika_sprite, self.walls)
+        self.physics_quentin = arcade.physics_engines.PhysicsEnginePlatformer(self.quentin_sprite, self.walls, .2)
 
+    def active_sprite(self):
+        if self.erika_active:
+            return self.erika_sprite
+        else:
+            return self.quentin_sprite
 
     def on_draw(self):
         arcade.start_render()
@@ -65,30 +74,42 @@ class Game(arcade.Window):
         self.walls.draw()
 
     def update(self, delta_time):
-        self.physics_engine.update()
+        self.physics_erika.update()
+        self.physics_quentin.update()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.D:
-            self.erika_sprite.change_x = 3
+            self.active_sprite().change_x = 3
 
-        if key == arcade.key.W and self.physics_engine.can_jump():
-            self.erika_sprite.change_y = 10
+        if key == arcade.key.A:
+            self.active_sprite().change_x = -3
 
-            
+        if key == arcade.key.W:
+            if self.erika_active and self.physics_erika.can_jump():
+                self.erika_sprite.change_y = 10
+            if self.quentin_active and self.physics_quentin.can_jump():
+                self.quentin_sprite.change_y = 10
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.Q:
             arcade.set_background_color(arcade.color.AERO_BLUE)
-            self.erika_sprite.alpha = 0
+            self.quentin_active = True
             self.quentin_sprite.alpha = 1
+            self.erika_active = False
+            self.erika_sprite.alpha = 0
 
         if key == arcade.key.E:
             arcade.set_background_color(arcade.color.AMAZON)
-            self.quentin_sprite.alpha = 0
+            self.erika_active = True
             self.erika_sprite.alpha = 1
+            self.quentin_active = False
+            self.quentin_sprite.alpha = 0
 
         if key == arcade.key.D:
-            self.erika_sprite.change_x = 0
+            self.active_sprite().change_x = 0
+
+        if key == arcade.key.A:
+            self.active_sprite().change_x = 0
 
         
 
